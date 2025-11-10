7a44b41701c851c67a12f0b21efe258d2873257e
as324279 <as324275@naver.com>
2025-11-10 15:34:23 +0900

    3-13

--- src/main/java/com/example/sbb/question/QuestionService.java
+++ src/main/java/com/example/sbb/question/QuestionService.java
@@ -5,7 +5,13 @@ import org.springframework.stereotype.Service;
 
 import com.example.sbb.DataNotFoundException;
 import com.example.sbb.user.SiteUser;
-
+import com.example.sbb.answer.Answer;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
 import java.time.LocalDateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 
 @RequiredArgsConstructor
 @Service
@@ -44,11 +51,12 @@ public class QuestionService {
 		this.questionRepository.save(q);
 	}
 	
-	public Page<Question> getList(int page) {
+	public Page<Question> getList(int page, String kw) {
 		List<Sort.Order> sorts = new ArrayList<>();
 		sorts.add(Sort.Order.desc("createDate"));
 		Pageable pageable = PageRequest.of(page, 10,Sort.by(sorts));
-		return this.questionRepository.findAll(pageable);
+		Specification<Question> spec = search(kw);
+		return this.questionRepository.findAll(spec,pageable);
 	}
 	
 	public void modify(Question question, String subject, String content) {
@@ -65,4 +73,23 @@ public class QuestionService {
 		question.getVoter().add(siteUser);
 		this.questionRepository.save(question);
 	}
+	
+	private Specification<Question> search(String kw) {
+		return new Specification<>() {
+			private static final long serialVersionUID = 1L;
+			public Predicate toPredicate(Root<Question> q, CriteriaQuery<?> query, CriteriaBuilder cb) {
+				query.distinct(true);
+				Join<Question, SiteUser> u1 = q.join("author",JoinType.LEFT);
+				Join<Question,Answer> a = q.join("answerList",JoinType.LEFT);
+				Join<Answer,SiteUser> u2 = a.join("author",JoinType.LEFT);
+				return cb.or(cb.like(q.get("subject"),  "%" + kw + "%"),
+						cb.like(q.get("content"), "%" + kw + "%"),
+						cb.like(u1.get("username"), "%" + kw + "%"),
+						cb.like(a.get("content"), "%" + kw + "%"),
+						cb.like(u2.get("username"), "%" + kw + "%"));
+						
+						
+			}
+	};
+}
 }
